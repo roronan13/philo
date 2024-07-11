22b6b37a69ae6016bd5ecaf7a7e5bdb6fa173142
roronan13 <ronan.pothier@gmail.com>
2024-07-11 21:48:34 +0200

    11 juil

--- philo.h
+++ philo.h
@@ -1,31 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/11 19:46:57 by rpothier          #+#    #+#             */
-/*   Updated: 2024/07/11 21:44:48 by rpothier         ###   ########.fr       */
+/*   Created: 2024/07/11 19:47:21 by rpothier          #+#    #+#             */
+/*   Updated: 2024/07/11 21:09:59 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include <pthread.h>
+#include <stdio.h>
 
-void	*hello(void *arg)
-{
-	int *arg2;
-	arg2 = (int *)arg;
-	
-	printf("Hello %d\n", *arg2);
-}
-
-int	main(int argc, char **argv)
-{
-	pthread_t id;
-	int		a;
-	a = 4;
-	
-	pthread_create(&id, NULL, hello, &a);
-	printf("id : %ld\n", id);
-}
