e7482a120b9860cb17b71b00710c0ac815238174
roronan13 <ronan.pothier@gmail.com>
2024-09-26 19:29:48 +0200

    26 sep

--- setup.c
+++ setup.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 16:29:40 by rpothier          #+#    #+#             */
-/*   Updated: 2024/09/26 19:13:07 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/09/26 19:15:58 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,17 @@ int	set_args(int argc, char **argv, int *tab_args)
 {
 	int	i;
 
-	i = 0;
+	i = 1;
 	tab_args = malloc(sizeof(int) * (argc - 1));
 	if (!tab_args)
 	{
 		printf("Error\nMalloc failed !\n");
 		return (1);
 	}
-	while ()
+	while (i < argc)
+	{
+		tab_args[i - 1] = ft_atoi(argv[i]);
+		i++;
+	}
 	
 }
